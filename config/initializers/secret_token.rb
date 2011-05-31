d3ba75a251a90803dd3dd0065ecf6b5a8e348b1a
Astr0surfer <astrosurfer@hotmail.it>
2011-05-30 18:11:47 -0700

    prima versione CMS di Astr0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Astr0ror::Application.config.secret_token = '7e4a30a9648e70753ca9891a4f671977253ba00f8ca6f9b6f79da879197f20bb2728753b0a7a563eb16615f59675ab887d2f54984fe199aea5fa8cafc18b7be2'
